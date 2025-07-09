05101f9164b657524042f696e203d21b9e6ecb19
vlad-website <vlad.website33@gmail.com>
2025-07-09 14:55:52 +0200

    Update config.py
    изменение порта

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-PORT = int(os.getenv("PORT", 8080))
+PORT = int(os.getenv("PORT", 10000))
 DATABASE_URL = os.getenv("DATABASE_URL")
