b5a2ded3e265ec20d78f826cfae5ef5584ae1353
vlad-website <vlad.website33@gmail.com>
2025-07-09 15:04:04 +0200

    Update config.py
    хардкодинг

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # -------------------------
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-PORT = int(os.getenv("PORT", 10000))
+BOT_TOKEN = "8110514255:AAGS6Zv_h68lgs_NSXDaMCWthPpVscz6q4E"
+WEBHOOK_URL = f"https://connectory-bot.onrender.com/8110514255:AAGS6Zv_h68lgs_NSXDaMCWthPpVscz6q4E"
+PORT = 10000
 DATABASE_URL = os.getenv("DATABASE_URL")
