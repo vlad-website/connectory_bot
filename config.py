3ef82b999e68cc47f57ab86fe478c136a9dde1c9
vlad-website <vlad.website33@gmail.com>
2025-07-09 13:25:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# File: config.py
+# -------------------------
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = int(os.getenv("PORT", 8080))
+DATABASE_URL = os.getenv("DATABASE_URL")
