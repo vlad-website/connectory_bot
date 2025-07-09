b40df5d67062c79be06e9677d3ab71c08239fc73
vlad-website <vlad.website33@gmail.com>
2025-07-09 13:27:25 +0200

    Update messages.py

--- handlers/messages.py
+++ handlers/messages.py
@@ -1,1 +1,16 @@
+# Folder: handlers/messages.py
+# -------------------------
+from telegram import Update
+from telegram.ext import ContextTypes
+from db.user_queries import get_user
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    user = await get_user(user_id)
+
+    if not user:
+        await update.message.reply_text("/start, пожалуйста.")
+        return
+
+    await update.message.reply_text(f"Принято: {update.message.text}")
 
