4fc17e371dcf143843ffe1e8366452f1576479ef
vlad-website <vlad.website33@gmail.com>
2025-07-25 18:00:01 +0200

    Update topics.py
    добавлены игры

--- core/topics.py
+++ core/topics.py
@@ -1,7 +1,7 @@
 TOPICS = {
     "it": ["programming", "design", "ai", "career_it", "gamedev", "freelance_it", "cybersecurity"],
     "education_science": ["neuroscience", "astronomy", "biology", "history", "philosophy", "self_dev"],
-    "hobbies": ["games", "travel", "books", "music"],
+    "games": ["hogwarts", "starfield", "diablo_4", "world_of_tanks", "red_dead_redemption", "world_of_warcraft", "fortnite", "minecraft", "gta5", "cs2", "valorant", "roblox", "pubg", "genshin", "lol", "dota2", "cod", "clash_royale", "apex", "among_us", "free_fire", "brawl_stars", "fifa", "rocket_league", "hearthstone", "elden_ring"],
     "business": ["startups", "partners", "marketing", "finance", "entrepreneurship", "investments", "crypto", "management"],
     "arts_culture": ["modern_art", "painting", "architecture", "photography", "theatre", "fashion", "culturology", "poetry"],
     "books": ["fiction", "nonfiction", "fantasy", "scifi", "biography", "poetry", "classics"],
