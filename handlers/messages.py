26dfdeacb0b52ff33781e93e59db236d5f6e3fe1
vlad-website <vlad.website33@gmail.com>
2025-08-08 18:11:29 +0200

    Update messages.py
    другая кнопка

--- handlers/messages.py
+++ handlers/messages.py
@@ -191,7 +191,7 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await remove_from_queue(user_id)
             
             await update.message.reply_text(
-                await tr(user, "search_stop"),
+                await tr(user, "search_stopped"),
                 reply_markup=await kb_after_sub(user)
             )
             await update_user_state(user_id, "menu_after_sub")
