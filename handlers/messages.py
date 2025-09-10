7de103cb8e4c7ce47d973713e3122b119419be1a
Web-Artisan <vlad.website33@gmail.com>
2025-09-10 17:57:01 +0200

    Update messages.py
    доработка

--- handlers/messages.py
+++ handlers/messages.py
@@ -222,21 +222,21 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         elif text == await tr(user, "btn_change_sub"):
             await remove_from_queue(user_id)
-            await update.message.reply_text(await tr(user, "search_stopped"))
-
-            await update_user_state(user_id, "sub")
-            sub_keys = TOPICS[user["theme"]] + ["any_sub"]
-            subtopics = [await tr(user, s) for s in sub_keys]
             await update.message.reply_text(
-                await tr(user, "choose_sub"),
-                reply_markup=ReplyKeyboardMarkup([[s] for s in subtopics], resize_keyboard=True)
+                await tr(user, "search_stopped"),
+                reply_markup=ReplyKeyboardMarkup(
+                    [[await tr(user, s)] for s in TOPICS[user["theme"]] + ["any_sub"]],
+                    resize_keyboard=True
+                )
+            )
+            await update_user_state(user_id, "sub")
+            await update.message.reply_text(
+                await tr(user, "choose_sub")
             )
             return
 
         elif text == await tr(user, "btn_main_menu"):
             await remove_from_queue(user_id)
-            await update.message.reply_text(await tr(user, "search_stopped"))
-
             await update_user_state(user_id, "menu")
             user = await get_user(user_id)
             from handlers.keyboards import kb_main_menu
